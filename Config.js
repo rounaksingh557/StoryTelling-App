9dd045729d5e4745bb8a39b3612b5e2f9610591b
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-05-18 15:37:32 +0530

    Completed Google Cloud Work

---
+++ Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXTo8tG-fDxIkJy10kzaHsQuSfsqnJaPE",
+  authDomain: "storytelling-a9a4e.firebaseapp.com",
+  projectId: "storytelling-a9a4e",
+  storageBucket: "storytelling-a9a4e.appspot.com",
+  messagingSenderId: "889110534035",
+  appId: "1:889110534035:web:183c2a010b1effe004ba66",
+};
