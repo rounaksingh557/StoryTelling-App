8aa842d625d4cd8f70ee00da82a735595b9e7911
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-05-19 18:54:34 +0530

    Switch Component, Firebase Updated for theme on switch change

--- Screens/Profiles.js
+++ Screens/Profiles.js
@@ -1,18 +1,178 @@
+// Modules Import
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Switch, StyleSheet, Image } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import firebase from "firebase";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
 
 export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      isEnabled: false,
+      light_theme: true,
+      profile_image: "",
+      name: "",
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  async fetchUser() {
+    let theme, name, image;
+    await firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", (snapshot) => {
+        theme = snapshot.val().current_theme;
+        name = `${snapshot.val().first_name} ${snapshot.val().last_name}`;
+        image = snapshot.val().profile_picture;
+      });
+
+    this.setState({
+      light_theme: theme === "light" ? true : false,
+      isEnabled: theme === "light" ? false : true,
+      name: name,
+      profile_image: image,
+    });
+  }
+
+  toggleSwitch() {
+    const previousState = this.state.isEnabled;
+    const theme = previousState ? "light" : "dark";
+    const updates = {};
+    updates["/users/" + firebase.auth().currentUser.uid + "/current_theme"] =
+      theme;
+    firebase.database().ref().update(updates);
+    this.setState({
+      isEnabled: !previousState,
+      light_theme: previousState,
+    });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
   render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Profile Page</Text>
-      </View>
-    );
+    if (this.state.fontsLoaded) {
+      return (
+        <View style={styles.container}>
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              />
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Story Telling App</Text>
+            </View>
+          </View>
+
+          <View style={styles.ScreenContainer}>
+            <View style={styles.profileImageContainer}>
+              <Image
+                source={{ uri: this.state.profile_image }}
+                style={styles.profileImage}
+              />
+              <Text style={styles.nameText}>{this.state.name}</Text>
+            </View>
+
+            <View style={styles.themeContainer}>
+              <Text style={styles.themeText}>Dark Theme</Text>
+              <Switch
+                style={{
+                  transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }],
+                }}
+                trackColor={{ false: "#767577", true: "white" }}
+                thumbColor={this.state.isEnabled ? "#fc8403" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={() => this.toggleSwitch()}
+                value={this.state.isEnabled}
+              />
+            </View>
+            <View style={{ flex: 0.3 }} />
+          </View>
+          <View style={{ flex: 0.07 }} />
+        </View>
+      );
+    } else {
+      return <AppLoading />;
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  appTitle: {
+    flex: 0.1,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitleTextContainer: {
+    flex: 0.8,
+    justifyContent: "center",
+    marginLeft: RFValue(9),
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+  },
+  ScreenContainer: {
+    flex: 0.9,
+  },
+  profileImageContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileImage: {
+    width: RFValue(150),
+    height: RFValue(150),
+    borderRadius: RFValue(75),
+    marginBottom: RFValue(20),
+  },
+  nameText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(15),
+  },
+  themeContainer: {
+    flex: 0.1,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: RFValue(20),
+    alignItems: "center",
+  },
+  themeText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15),
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
